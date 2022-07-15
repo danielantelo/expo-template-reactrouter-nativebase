cc5fad5c0d1211d2ddd3e94850d797a75d372efb
Dan <daniel.antelo@oaknorth.com>
2022-07-15 10:22:23 +0100

    clean up

--- App.tsx
+++ App.tsx
@@ -2,30 +2,21 @@ import { Route, Routes } from 'react-router';
 import { NativeBaseProvider } from 'native-base';
 
 import { nativeBaseTheme } from './theme';
-
 import { Destination, Router } from './utils/routing';
-import { Loader } from './components/Loader';
-
 import About from './screens/About';
 import Home from './screens/Home';
 import Form from './screens/Form';
 
 export default function App() {
-  const activeProgramLoaded = true;
-
   return (
     <NativeBaseProvider theme={nativeBaseTheme}>
-      {!activeProgramLoaded ? (
-        <Loader />
-      ) : (
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path={Destination.About} element={<About />} />
-            <Route path={Destination.Form} element={<Form />} />
-          </Routes>
-        </Router>
-      )}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path={Destination.About} element={<About />} />
+          <Route path={Destination.Form} element={<Form />} />
+        </Routes>
+      </Router>
     </NativeBaseProvider>
   );
 }
