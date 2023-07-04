2ea13483b0b1c25c162a883c1ec3fdd669c4f60b
Daniel Antelo <danielantelo@live.com>
2023-07-04 11:11:53 +0100

    fix lint

--- apps/myapp/webpack.config.js
+++ apps/myapp/webpack.config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { createWebpackConfigAsync } = require('expo-yarn-workspaces/webpack');
 
 module.exports = async function (env, argv) {
